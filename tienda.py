030e5d003f1930bc98dd92b33ee34b218ea98d55
olich797 <olich.castro@gmail.com>
2025-09-10 17:25:19 -0400

    bug

--- tienda.py
+++ tienda.py
@@ -284,7 +284,7 @@ if st.session_state.pagina == "Proforma":
                 item["Cantidad"] = st.number_input(f"Cantidad", min_value=1, step=1, value=item["Cantidad"], key=f"cantidad_{i}")
 
             with col3:
-                item["Precio Unitario BOB"] = st.number_input(f"Precio Unitario", min_value=0.01, step=0.01, value=item["Precio Unitario BOB"], key=f"precio_{i}")
+                item["Precio Unitario BOB"] = st.number_input(f"Precio Unitario", min_value=0.01, step=0.01, value=item["Precio Unitario BOB"], key=f"precio_bs_{i}")
 
             item["Precio Total BOB"] = round(item["Cantidad"] * item["Precio Unitario BOB"], 2)
 
