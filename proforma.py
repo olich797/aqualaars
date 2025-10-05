4c560537bad1576ca65054a61c751c6e044c1a69
olich797 <olich.castro@gmail.com>
2025-10-05 04:43:48 -0400

    correcion

--- proforma.py
+++ proforma.py
@@ -97,28 +97,28 @@ def generar_proforma(db):
             db.collection("proformas").document(proforma_id).set(proforma_data)
             st.success(f"✅ La proforma ha sido guardada en Firebase con ID: {proforma_id}")
 
-            fig, ax = plt.subplots(figsize=(8.5, 11))  # Carta vertical
+            fig, ax = plt.subplots(figsize=(7.5, 10))  # Carta vertical
 
-            ax.set_title("Proforma", fontsize=24, fontweight="bold", pad=10)
+            ax.set_title("Proforma", fontsize=22, fontweight="bold", pad=10)
 
             # Marca de agua
-            ax.text(0.5, 0.5, "Aqualaars", font="Arial", fontweight="bold", fontsize=90,
+            ax.text(0.5, 0.5, "Aqualaars", font="Arial", fontweight="bold", fontsize=80,
                     color="#00BFFF", alpha=0.2, ha="center", va="center", transform=ax.transAxes)
-            ax.text(0.60, 0.42, "Todo para su piscina", font="Arial", fontweight="bold", fontsize=30,
+            ax.text(0.60, 0.42, "Todo para su piscina", font="Arial", fontweight="bold", fontsize=25,
                     color="#00BFFF", alpha=0.2, ha="center", va="center", transform=ax.transAxes)
 
             # Datos del cliente (más compactos)
-            ax.text(0, 0.85, f"Nombre: {nombre_cliente}         CI/NIT: {ci_nit}", fontsize=12)
-            ax.text(0, 0.82, f"Fecha emisión: {fecha_actual.strftime('%Y-%m-%d')}", fontsize=12)
-            ax.text(0, 0.79, f"Fecha vencimiento: {fecha_vencimiento.strftime('%Y-%m-%d')}", fontsize=12)
+            ax.text(0, 0.85, f"Nombre: {nombre_cliente}         CI/NIT: {ci_nit}", fontsize=10)
+            ax.text(0, 0.82, f"Fecha emisión: {fecha_actual.strftime('%Y-%m-%d')}", fontsize=10)
+            ax.text(0, 0.79, f"Fecha vencimiento: {fecha_vencimiento.strftime('%Y-%m-%d')}", fontsize=10)
 
             col_labels = ["Producto", "Cantidad", "Precio Unitario BOB", "Precio Total BOB"]
             table_data = [[item["Nombre"], item["Cantidad"], item["Precio Unitario BOB"], item["Precio Total BOB"]] for item in st.session_state.productos_lista]
             tabla = ax.table(cellText=table_data, colLabels=col_labels, cellLoc="center", loc="center", colWidths=[0.50, 0.13, 0.18, 0.18])
             tabla.auto_set_font_size(False)
-            tabla.set_fontsize(10)
+            tabla.set_fontsize(9)
             tabla.scale(1.2, 1.2)
-            ax.text(0, 0.28, f"Total: {round(total_proforma, 2)} BOB", fontsize=14, fontweight="bold")
+            ax.text(0, 0.28, f"Total: {round(total_proforma, 2)} BOB", fontsize=12, fontweight="bold")
             ax.axis("off")
             st.pyplot(fig)
 
