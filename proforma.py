7836d1b9a3a86288ac94e372bf2ee273ba9a1aee
olich797 <olich.castro@gmail.com>
2025-10-05 04:37:18 -0400

    Update proforma.py

--- proforma.py
+++ proforma.py
@@ -118,7 +118,7 @@ def generar_proforma(db):
             tabla.auto_set_font_size(False)
             tabla.set_fontsize(10)
             tabla.scale(1.2, 1.2)
-            ax.text(0, 0.35, f"Total: {round(total_proforma, 2)} BOB", fontsize=14, fontweight="bold")
+            ax.text(0, 0.30, f"Total: {round(total_proforma, 2)} BOB", fontsize=14, fontweight="bold")
             ax.axis("off")
             st.pyplot(fig)
 
